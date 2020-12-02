4167f184633884f8f7956d165c87701af236648b
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-12-01 19:20:34 -0500

    Removed login requirement

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
 #Coded completely by smokeytube
-from selenium import webdriver
 import time
 from tkinter import *
 import pyautogui
+from datetime import datetime
+
+
 
 
 def click(image):
         start = pyautogui.locateOnScreen(image, confidence=0.01)
-        print (image)
         pyautogui.moveTo(start)
         pyautogui.click()
         time.sleep(0.001)
 
 def imageProcess(image):
-        print ("imageProcess")
         return pyautogui.locateOnScreen(image, region=(400,400,200,600), confidence=0.8)
 
-userw = input("Discord email: ")
-passw = input("Discord password: ")
-
-CHROME_DVR_DIR = '/webdrivers/chromedriver.exe'
-driver = webdriver.Chrome(CHROME_DVR_DIR)
-
-driver.get('https://discord.com/login?redirect_to=%2Fchannels%2F%40me')
-
-usernameinput = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[1]/div/div[2]/input')
-passwordinput = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[2]/div/input')
-submitbutn = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]')
-
-usernameinput.send_keys(userw)
-passwordinput.send_keys(passw)
-submitbutn.click()
-
-time.sleep(5)
 
 # x = 15
 # print ("You have 15 seconds to select the server and channel to scan")
@@ -41,7 +24,9 @@ time.sleep(5)
 #     time.sleep(1)
 #     x = x-1
 
-accept = 'hello.png'
+accept = '/media/hello.png'
+now = datetime.now()
+x = 0
 
 print ("Scan initiated")
 while True:
@@ -51,3 +36,7 @@ while True:
         time.sleep(69420)
     else:
         print ("Could not locate. Trying again")
+        current_time = now.strftime("%H%M")
+        if current_time == current_time + 1:
+        x = x + 1
+
